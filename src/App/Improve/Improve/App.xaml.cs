39dd3203bd68756c6c8f875236b628ac4ab70f35
sunyubo <yubo.sun@dbh.dynabook.com>
2020-10-20 16:33:54 +0800

    添加prism框架

--- src/App/Improve/Improve/App.xaml.cs
+++ src/App/Improve/Improve/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +13,17 @@ namespace Improve
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            var mainWin = Container.Resolve<MainWindow>();
+            return mainWin;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
     }
 }
