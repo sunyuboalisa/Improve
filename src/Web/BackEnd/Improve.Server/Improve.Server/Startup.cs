b6a9979e66153ebddedd6b3da8573af21e0d5dab
sunyubo <sunyubo.alisa@outlook.com>
2020-10-27 17:29:14 +0800

    添加swagger

--- src/Web/BackEnd/Improve.Server/Improve.Server/Startup.cs
+++ src/Web/BackEnd/Improve.Server/Improve.Server/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Improve.Server.Models;
 using Microsoft.AspNetCore.Builder;
@@ -12,6 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace Improve.Server
 {
@@ -33,7 +37,36 @@ namespace Improve.Server
                 options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-           
+
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +76,14 @@ namespace Improve.Server
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+            app.UseSwagger(c => {
+                c.SerializeAsV2 = true;
+            });
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api v1");
+            });
 
             app.UseHttpsRedirection();
 
@@ -54,6 +95,8 @@ namespace Improve.Server
             {
                 endpoints.MapControllers();
             });
-        }
+
+            
+        }   
     }
 }
