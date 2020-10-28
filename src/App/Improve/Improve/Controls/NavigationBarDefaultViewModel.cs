b0d53d5afa3ee7b90204d1e0f9759fec81b379eb
sunyubo <sunyubo.alisa@outlook.com>
2020-10-28 10:27:23 +0800

    更改主页面布局 删除不需要的项目引用。

---
+++ src/App/Improve/Improve/Controls/NavigationBarDefaultViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Improve.Controls
+{
+    class NavigationBarDefaultViewModel: BindableBase
+    {
+    }
+}
