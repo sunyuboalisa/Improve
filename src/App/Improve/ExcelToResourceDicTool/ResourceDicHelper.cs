df03c789106fa7dda1744735f2feb9ca748d1547
Cmotal <370549403@qq.com>
2020-11-18 08:34:36 +0800

    11/18 提交

---
+++ src/App/Improve/ExcelToResourceDicTool/ResourceDicHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Threading;
+
+namespace ExcelToResourceDicTool
+{
+    class ResourceDicHelper
+    {
+        public static bool GenerateResourceDicByExcel(string excelPath,string resourceDicPath)
+        {
+            bool res = false;
+            var testPath = @"tt.xaml";
+            ResourceDictionary resourceDictionary = new ResourceDictionary();
+            var s = resourceDictionary.FindName("ResourceDictionary");
+            resourceDictionary.Add("rr", 4444);
+            
+            using (var fs = new StreamWriter(testPath))
+            {
+                XamlWriter.Save(resourceDictionary, fs);
+            }
+
+            return res;
+        }
+    }
+}
